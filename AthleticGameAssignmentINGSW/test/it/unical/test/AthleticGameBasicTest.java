41c05df751de73e0e92a6767e9acf34bc625e432
sciam <sciam@160.97.121.1>
2017-10-18 13:16:42 +0200

    update

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,29 @@
 package it.unical.test;
 
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
+
 public class AthleticGameBasicTest {
 
+	@Test
+	public void addArrivalWorks() {
+	
+		AthleticGame athleticGame = new AthleticGame("Corsa");
+		athleticGame.addArrival("Marco", Instant.now());
+		Assert.assertEquals(1, athleticGame.getArrivals().size());
+		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getParecipiantTimeWorks() {
+		
+		AthleticGame athleticGame = new AthleticGame("Corsa");
+		athleticGame.addArrival("ciccio", Instant.now());
+		athleticGame.getParecipiantTime("Franco");
+	}
 }
